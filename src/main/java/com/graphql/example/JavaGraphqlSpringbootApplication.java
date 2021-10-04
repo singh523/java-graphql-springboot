64704d48f283f0d4fb6bcafed34cf1ab5aebbf0a
kjadsflj asdfsdf <test@asdfsadfsf.com>
2021-10-04 18:56:12 +1100

    Initial changes

---
+++ src/main/java/com/graphql/example/JavaGraphqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGraphqlSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/example/JavaGraphqlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphqlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
